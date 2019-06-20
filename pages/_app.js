4eb8a274c5d5ff785dbc812efc78fb0f979ef112
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2019-06-19 21:33:33 -0400

    next config and _app.js

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Simple PIM</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <AppProvider apiKey={API_KEY}>
+          <Component {...pageProps} />
+        </AppProvider>
+          
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
