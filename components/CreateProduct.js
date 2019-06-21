9fa0db27c67bb351d5c39c4b8710e50bb8bc5d2a
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2019-06-21 16:26:04 -0400

    created dynamic Create Product component

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,4 +1,4 @@
-import { TextField, Layout, Card, FormLayout } from '@shopify/polaris';
+import { TextField, Layout, Card, FormLayout, TextContainer } from '@shopify/polaris';
 
 class CreateProduct extends React.Component {
   state = {
@@ -12,25 +12,59 @@ class CreateProduct extends React.Component {
   };  
 
   render() {
-    const { description } = this.state;
-    const { title } = this.state;
-    const { price } = this.state;
+    
 
     return (
       <Layout sectioned={true}>
       <Layout.AnnotatedSection 
         title="Create a Product" 
-        description="Use the Product API GET endpoint to create a new product."
+        description="Use the Product API POST endpoint to create a new product."
       >
         <Card
           sectioned 
           primaryFooterAction={{content: 'Create Product'}}
         >
-          <FormLayout>
-            <TextField label="Title" onChange={this.handleChange('title')} value={this.state.title}/>
-            <TextField label="Description" multiline onChange={this.handleChange('description')} value={this.state.description} />
-            <TextField label="Price" type="number" onChange={this.handleChange('price')} value={this.state.price} />
-          </FormLayout>
+          <Card.Section>
+            <FormLayout>
+              <TextField label="Title" onChange={this.handleChange('title')} value={this.state.title}/>
+              <TextField label="Description" multiline onChange={this.handleChange('description')} value={this.state.description} />
+              <TextField label="Price" type="number" onChange={this.handleChange('price')} value={this.state.price} />
+            </FormLayout>
+          </Card.Section>
+          <Card.Section>
+            <TextContainer>
+              <p>POST /admin/products.json</p>
+              <p>{"{"}</p>
+              <p className="indent1" >
+                {'"'}title{'"'}: {'"'}{this.state.title}{'"'},
+              </p>
+              <p className="indent1" >
+                {'"'}body_html{'"'}: {'"'}{this.state.description}{'"'},
+              </p>
+              <p className="indent1" >
+                {'"'}variants{'"'}: [
+              </p>
+              <p className="indent2" >
+                {'{'}
+              </p>
+              <p className="indent3" >
+                {'"'}price{'"'}: {'"'}{this.state.price}{'"'}
+              </p>
+              <p className="indent2" >
+                {'}'}
+              </p>
+              <p className="indent1" >
+                ]
+              </p>
+              <p className="indent0" >
+                {"}"}
+              </p>
+
+              
+              
+            </TextContainer>
+          </Card.Section>
+          
         </Card>
       </Layout.AnnotatedSection>
     </Layout>
